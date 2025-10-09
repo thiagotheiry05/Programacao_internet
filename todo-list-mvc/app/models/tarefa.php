469dc0ec7014fece37e367d80d31921b3b935da9
thiagotheiry05 <thiago.thierry.17@gmail.com>
2025-10-09 11:40:47 -0300

    subuindo o arquivo do controller

--- todo-list-mvc/app/models/tarefa.php
+++ todo-list-mvc/app/models/tarefa.php
@@ -9,7 +9,37 @@ class Tarefa{
         $db = new Database(); 
         $this->conn = $db->conectar();
     }
-}
 
+    ## Listar 
+
+    public function listar(){
+        $tarefas = []; 
+        $sql = "SELECT * FROM tarefas ORDER BY data_criacao DESC"; 
+        $resultado = $this->conn->query($sql);
+
+        if($resultado -> num_rows>0){
+            while($row = $resultado->fetch_assoc())
+                $tarefas[] = $row; 
+        }
+
+        return $tarefas; 
+    }
+
+    ## Criar 
+
+    public function criar($descricao){
+        $descricao = $this->conn->real_escape_string($descricao); 
+        $sql = "INSERT INTO tarefas (descricao) VALUES ('$descricao')";
+        return $this->conn->query($sql);
+    }
+
+    ## Excluir 
+
+    public function excluir($id){
+        $id = intval($id); 
+        $sql = "DELETE FROM tarefas WHERE id = $id"; 
+        return $this->conn->query($sql); 
+    }
+}
 
 ?>
